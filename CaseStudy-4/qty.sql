9a4f8a56f345a8d683f004320713a5390ed250ea
AveticsRobotics <73038067+Junszz@users.noreply.github.com>
2022-10-19 12:02:25 +0800

    Update sql

--- CaseStudy-4/qty.sql
+++ CaseStudy-4/qty.sql
@@ -1,15 +1,16 @@
 create table quantity
 ( 
     coffeeid int unsigned not null auto_increment primary key,
-    coffeename char(30) not null,
+    coffeecat char(30) not null,
+    coffeetype char(20) not null,
     coffeeqty int unsigned not null
 );
 
 use javajam;
 
 insert into quantity values
-    (1, 'Just Java', 1),
-    (2, 'Cafe au Lait', 1),
-    (3, 'Cafe au Lait', 1),
-    (4, 'Iced Cappuccino', 1),
-    (5, 'Iced Cappuccino', 1); 
+    (1, 'Just Java','Just Java', 1),
+    (2, 'Cafe au Lait','Single', 1),
+    (3, 'Cafe au Lait','Double', 1),
+    (4, 'Iced Cappuccino','Single', 1),
+    (5, 'Iced Cappuccino', 'Double',1); 
